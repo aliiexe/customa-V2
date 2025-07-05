b3e30e5c7a4c424bb8e3d4948f31b0341e773a49
Aboubakr-KETOUN <aboubakrketoun@gmail.com>
2025-07-05 19:46:16 +0100

    attempting signup fix

--- app/login/[[...rest]]/page.tsx
+++ app/login/[[...rest]]/page.tsx
@@ -5,7 +5,12 @@ import Link from "next/link";
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center flex-col">
-      <SignIn path="/login" routing="path" signUpUrl="/sign-up" />
+      <SignIn 
+        path="/login" 
+        routing="path" 
+        signUpUrl="/sign-up"
+        redirectUrl="/"
+      />
     </div>
   );
 }
--- app/sign-up/[[...rest]]/page.tsx
+++ app/sign-up/[[...rest]]/page.tsx
@@ -8,8 +8,7 @@ export default function SignUpPage() {
         path="/sign-up"
         routing="path"
         signInUrl="/login"
-        afterSignUpUrl="/"
-        afterSignInUrl="/"
+        redirectUrl="/"
       />
     </div>
   );
